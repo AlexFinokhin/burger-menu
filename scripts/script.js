95ea952763d2ab95f79936633a072083dfc3d952
Alex Finokhin <finokhinalex@yandex.ru>
2023-07-31 23:16:38 +0300

    upd

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,73 @@
 document.getElementById("main-action-button").onclick = function () {
-    document.getElementById("products").scrollIntoView({ behavior: "smooth" });
+  document.getElementById("products").scrollIntoView({ behavior: "smooth" });
 };
 
 const links = document.querySelectorAll(".menu-item > a");
 for (let i = 0; i < links.length; i++) {
-    links[i].onclick = function () {
-        document
-            .getElementById(links[i].getAttribute("data-link"))
-            .scrollIntoView({ behavior: "smooth" });
-    };
+  links[i].onclick = function () {
+    document
+      .getElementById(links[i].getAttribute("data-link"))
+      .scrollIntoView({ behavior: "smooth" });
+  };
 }
 
 const buttons = document.getElementsByClassName("product-button");
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function () {
-        document.getElementById("order").scrollIntoView({ behavior: "smooth" });
-    };
+  buttons[i].onclick = function () {
+    document.getElementById("order").scrollIntoView({ behavior: "smooth" });
+  };
 }
 
 const burger = document.getElementById("burger");
 const name = document.getElementById("name");
 const phone = document.getElementById("phone");
 document.getElementById("order-action").onclick = function () {
-    let hasError = false;
+  let hasError = false;
+  [phone, name, burger].forEach((item) => {
+    if (!item.value) {
+      item.parentElement.style.background = "red";
+      hasError = true;
+    } else {
+      item.parentElement.style.background = "";
+    }
+  });
+
+  if (!hasError) {
     [phone, name, burger].forEach((item) => {
-        if (!item.value) {
-            item.parentElement.style.background = "red";
-            hasError = true;
-        } else {
-            item.parentElement.style.background = "";
-        }
+      item.value = "";
     });
 
-    if (!hasError) {
-        [phone, name, burger].forEach((item) => {
-            item.value = "";
-        });
-
-        alert("Спасибо за заказ");
-    }
+    alert("Спасибо за заказ");
+  }
 };
 
 const prices = document.getElementsByClassName("products-item-price");
 document.getElementById("change-currency").onclick = function (e) {
-    const currentCurrency = e.target.innerText;
+  const currentCurrency = e.target.innerText;
 
-    let newCurrency = "$";
-    let coefficient = 1;
+  let newCurrency = "$";
+  let coefficient = 1;
 
-    if (currentCurrency === "$") {
-        newCurrency = "РУБ";
-        coefficient = 80;
-    } else if (currentCurrency === "РУБ") {
-        newCurrency = "BYN";
-        coefficient = 3;
-    } else if (currentCurrency === "BYN") {
-        newCurrency = "€";
-        coefficient = 0.9;
-    } else if (currentCurrency === "€") {
-        newCurrency = "¥";
-        coefficient = 6.9;
-    }
+  if (currentCurrency === "$") {
+    newCurrency = "РУБ";
+    coefficient = 80;
+  } else if (currentCurrency === "РУБ") {
+    newCurrency = "BYN";
+    coefficient = 3;
+  } else if (currentCurrency === "BYN") {
+    newCurrency = "€";
+    coefficient = 0.9;
+  } else if (currentCurrency === "€") {
+    newCurrency = "¥";
+    coefficient = 6.9;
+  }
 
-    e.target.innerText = newCurrency;
+  e.target.innerText = newCurrency;
 
-    for (let i = 0; i < prices.length; i++) {
-        prices[i].innerText =
-            +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) +
-            " " +
-            newCurrency;
-    }
+  for (let i = 0; i < prices.length; i++) {
+    prices[i].innerText =
+      +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) +
+      " " +
+      newCurrency;
+  }
 };
